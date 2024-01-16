b1f27d5a06956adfbf80ce570a23eadc85f8072b
Giorgos <46922189+0rionx7@users.noreply.github.com>
2024-01-16 19:16:22 +0200

    Update mongoDb.js

--- mongoDb.js
+++ mongoDb.js
@@ -4,7 +4,7 @@ import { Emitter } from '@socket.io/mongo-emitter';
 mongoose.set('strictQuery', false);
 mongoose
    .connect(
-  `mongodb+srv://orionx7:WloFovQdp9AYuOce@mean.c0rsp.mongodb.net/Text?retryWrites=true&w=majority`,
+  `mongodb+srv://primitivo:7ZuIFwncwAlka6oX@cluster0.qyvtcbt.mongodb.net/?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(
