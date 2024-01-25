8b0ed3e5b367711fbee394daa27ad0508982158a
Giorgos <46922189+0rionx7@users.noreply.github.com>
2024-01-25 10:35:57 +0200

    Update mongoDb.js

--- mongoDb.js
+++ mongoDb.js
@@ -21,7 +21,7 @@ mongoose.set('toJSON', { virtuals: true });
 const DB = 'Socket';
 const COLLECTION = 'socket.io-adapter-events';
 const conn2 = mongoose.createConnection(
-  `mongodb+srv://orionx7:i7HxupA9otqHGDWo@testcluster.t3fpgoc.mongodb.net/${DB}?retryWrites=true&w=majority`,
+   `mongodb+srv://primitivo:7ZuIFwncwAlka6oX@cluster0.qyvtcbt.mongodb.net/test?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
